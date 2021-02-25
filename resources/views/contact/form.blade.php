fa6b53305ca85e70af7e2ebd0ffc05e402af20b0
Yuriy Stolov <yuriystolov@gmail.com>
2021-02-25 21:43:48 +0500

    Add error handler

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -7,7 +7,13 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="contactModalLabel">Add new contact</h5>
+                <h5 class="modal-title" id="contactModalLabel">
+                    @if(isset($contact))
+                    Edit contact
+                    @else
+                    New contact
+                    @endif
+                </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -15,7 +21,7 @@
             <div class="modal-body">
                 @if(isset($contact))
                 <form id="contactForm" class="needs-validation" novalidate method="post">
-                    <input id="contactId" type="hidden" value="{{$contact->id}}">
+                    <input type="hidden" name="id" value="{{$contact->id}}">
                     @else
                     <form id="contactForm" class="needs-validation" novalidate method="put">
                         @endif
@@ -26,7 +32,7 @@
                                     <span class="input-group-text">Name</span>
                                 </div>
                                 <input name="name" id="contactName" type="text" class="form-control" required>
-                                <div class="invalid-feedback">
+                                <div class="invalid-feedback" data-default="Enter contact name">
                                     Enter contact name
                                 </div>
                             </div>
@@ -58,15 +64,59 @@
 </div>
 <script>
     $(function() {
+        function showErrorModal(textStatus, errorThrown) {
+            const modal = $('#errorModal');
+            modal.find('#errorModalLabel').html(textStatus);
+            modal.find('.modal-body').html(errorThrown);
+            modal.modal('show');
+        }
+
+        function resetFormValidation(form) {
+            form.removeClass('was-validated');
+            form.find('.invalid-feedback').each(function() {
+                $(this).html($(this).data('default'));
+            });
+            form.find(':input').each(function() {
+                this.setCustomValidity('');
+            });
+        }
+
+        function showInvalidField(form, data) {
+            const input = form.find(`:input`).filter(function() {
+                return this.value == data.value;
+            });
+            input.next('.invalid-feedback').html(data.message);
+            input[0].setCustomValidity(data.message);
+            form.removeClass('was-validated');
+            this.checkValidity();
+            form.addClass('was-validated');
+        }
+
+        function submitForm(form) {
+            $.ajax({
+                method: form.attr('method'),
+                url: `/contacts/{{ $contact->id ?? ''}}`,
+                data: form.serializeArray(),
+            }).done((data) => {
+                if (data.status) {
+                    window.location = window.location;
+                } else {
+                    showInvalidField(form, data);
+                }
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                showErrorModal(textStatus, errorThrown);
+            });
+        }
+
         $(document).on('click', '.btn-add', function(event) {
             event.preventDefault();
             const group = $(this).parents('.addable-group');
-            const currentItem = $(this).parents('.input-group:first');
-            const newItem = $(currentItem.clone()).appendTo(group);
-            currentItem.find('input').val('');
+            const input = $(this).parents('.input-group:first');
+            $(input.clone()).appendTo(group);
+            input.find('input').val('');
             group.find('.input-group:not(:last) .btn-add')
-                .removeClass('btn-add').addClass('btn-remove')
-                .removeClass('btn-success').addClass('btn-danger')
+                .removeClass('btn-add btn-success')
+                .addClass('btn-remove btn-danger')
                 .html('Remove');
         }).on('click', '.btn-remove', function(event) {
             $(this).parents('.input-group:first').remove();
@@ -75,25 +125,12 @@
         });
 
         $('#contactForm').submit(function(event) {
-            $(this).removeClass('was-validated');
+            resetFormValidation($(this));
             event.preventDefault();
             event.stopPropagation();
 
             if (!!this.checkValidity()) {
-                $.ajax({
-                    method: $(this).attr('method'),
-                    url: `/contact/{{ $contact->id ?? '' }}`,
-                    data: $(this).serializeArray(),
-                }).done(function(data) {
-                    if (data.status) {
-                        window.location = window.location;
-                    } else {
-                        alert(`${data.message}`);
-                    }
-                }).fail(function(jqXHR, textStatus, errorThrown) {
-                    alert(`${textStatus}: ${errorThrown}`);
-                });;
-
+                submitForm($(this));
                 return true;
             }
 
