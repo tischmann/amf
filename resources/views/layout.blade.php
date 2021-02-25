dd964b0f7719803ac65bebf3442b6d1399825769
Yuriy Stolov <yuriystolov@gmaile.com>
2021-02-25 09:58:24 +0500

    Add layout view

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+</head>
+
+<body>
+    @yield('content', 'Default content')
+</body>
+
+</html>
