4be9d6b5d06fb0427f24dbc2d4eb6301627c5881
Yuriy Stolov <yuriystolov@gmaile.com>
2021-02-25 10:28:57 +0500

    Optimizing views structure

---
+++ resources/views/contact/contact.blade.php
@@ -0,0 +1,44 @@
+@include('contact.notfound')
+
+@section('contact')
+
+@if (count($contacts) > 0)
+
+@foreach ($contacts as $contact)
+<tr>
+    <th scope="row" class="text-center align-middle">{{$contact->id}}</th>
+    <td class="text-center align-middle">{{$contact->name}}</td>
+    <td class="text-center align-middle">
+        @foreach ($contact->phones as $phone)
+        <div>{{$phone}}</div>
+        @endforeach
+    </td>
+    <td class="text-center align-middle">
+        @foreach ($contact->emails as $email)
+        <div>{{$email}}</div>
+        @endforeach
+    </td>
+    <td class="text-center align-middle">
+        <a href="/contact/{{$contact->id}}/edit" title="Изменить">
+            <svg class="bi" width="16" height="16" fill="currentColor">
+                <use xlink:href="assets/images/bootstrap-icons.svg#pencil-fill" />
+            </svg>
+        </a>
+    </td>
+    <td class="text-center align-middle">
+        <a href="/contact/{{$contact->id}}/delete" title="Удалить">
+            <svg class="bi" width="16" height="16" fill="currentColor">
+                <use xlink:href="assets/images/bootstrap-icons.svg#trash-fill" />
+            </svg>
+        </a>
+    </td>
+</tr>
+@endforeach
+
+@else
+
+@yield('notfound')
+
+@endif
+
+@endsection
---
+++ resources/views/contact/notfound.blade.php
@@ -0,0 +1,7 @@
+@section('notfound')
+<tr>
+    <td colspan="7" class="text-center align-middle">
+        <span class="text-center">Contacts not found</span>
+    </td>
+</tr>
+@endsection
